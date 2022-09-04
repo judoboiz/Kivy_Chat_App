e15619daa9e8ecb10279b7e04c9ea6a0f5f48ff2
devstijepo <86206504+judoboiz@users.noreply.github.com>
2022-09-04 16:15:59 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,27 @@
+from kivy.uix.label import Label
 from kivy.app import App
 from kivy.core.audio import SoundLoader
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
-class FrontPage(BoxLayout):
+class FrontPage(GridLayout):
 
-    def play_sound(slf):
+    def __init__(self, **kwargs):
+        super(FrontPage, self).__init__(**kwargs)
+
+        self.cols = 2
+
+        self.add_widget(Label(text="Name: "))
+
+        self.name = TextInput(multiline=False)
+        self.add_widget(self.name)
+
+        self.submit = Button(text= "Submit",font_size=32)
+        self.submit.bind(on_press =self.press)
+        self.add_widget(self.submit)
+
+    def play_sound():
         sound = SoundLoader.load("sound.mp3")
         if sound:
             sound.play()
@@ -24,7 +41,7 @@ class FrontPage(BoxLayout):
             return end_msg
 
 class MyApp(App):
-    def build():
+    def build(self):
         return FrontPage()
 
 if __name__ == "__main__":

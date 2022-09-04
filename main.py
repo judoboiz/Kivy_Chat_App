45e2df65223dbfd3ffb25e12db6b7654b9c18407
FatFatus <105380610+FatFatus@users.noreply.github.com>
2022-09-04 22:25:29 +0200

    started working on looks of app

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-from kivy.uix.widget import Widget
+from operator import length_hint
+from unicodedata import name
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
-class MainWinget(Widget):
-    pass
+class buttuon(App):
+    def build(self):
+        main_layout=BoxLayout()
 
-class TheLabApp(App):
-    pass
+        min= TextInput(text="Min", font_size=33, size_hint=(.1, .1), pos_hint={'center_x':.95, 'center_y':.95})
+        max= TextInput(text="Max", font_size=33, size_hint=(.1, .1),pos_hint={'center_x':.95, 'center_y':.95})
+        name= TextInput(text="name", font_size=33, size_hint=(.1, .1),pos_hint={'center_x':.95, 'center_y':.95})
+        
+        alt_layout=BoxLayout()
 
-TheLabApp().run()
+        alt_layout.add_widget(min)
+        alt_layout.add_widget(max)
+        alt_layout.add_widget(name)
+        main_layout.add_widget(alt_layout)
+
+        return main_layout
+
+
+
+    
+    
+  
+if __name__ == '__main__':
+
+    helloKivy = buttuon()
+    helloKivy.run()

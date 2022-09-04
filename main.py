617c6cb4d512eac428eab40c04f3015f9680a993
judoboiz <86206504+judoboiz@users.noreply.github.com>
2022-09-04 14:43:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-from kivy.uix.widget import Widget
 from kivy.app import App
+from kivy.core.audio import SoundLoader
+from kivy.uix.boxlayout import BoxLayout
 
-class MainWinget(Widget):
-    pass
+class FrontPage(BoxLayout):
 
-class TheLabApp(App):
-    pass
+    def play_sound(slf):
+        sound = SoundLoader.load("sound.mp3")
+        if sound:
+            sound.play()
+        else:
+            print("Fuck my self")
 
-TheLabApp().run()
+    def translertor(msg,type):
+        if type==True:
+            send_msg = ''.join(format(ord(i), '08b')for i in msg)
+            return send_msg
+        elif type == False:
+            end_msg = ""
+            for i in range(int(len(msg)/8)):
+                recive_msg = chr(int(msg[0+i*8:8+i*8],2))
+                end_msg = end_msg + recive_msg
+                print(recive_msg)
+            return end_msg
+
+class MyApp(App):
+    def build():
+        return FrontPage()
+
+if __name__ == "__main__":
+    MyApp().run()
